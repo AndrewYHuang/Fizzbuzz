c24ae924d722900d66bd20fcbf2b2c5b48fe1afb
Andrew Huang <Andrew.Huang@softwire.com>
2018-11-06 16:38:29 +0000

    Add Bang - 7

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,18 +10,22 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            string output;
-            for (int i = 1; i <= 100; i++)
+            for (var i = 1; i <= 100; i++)
             {
-                output = "";
-                if (i % 3 == 0)
+                var output = "";
+                if (i % 3 == 0) // Multiple of 3: Add Fizz
                 {
                     output += "Fizz";
                 }
 
-                if (i % 5 == 0)
+                if (i % 5 == 0) // Multiple of 5: Add Buzz
                 {
                     output += "Buzz";
+                }
+
+                if (i % 7 == 0) // Multiple of 7: Add Bang
+                {
+                    output += "Bang";
                 }
 
                 if (output == "")

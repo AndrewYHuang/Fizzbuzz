b79647f48e6e9f439476f7869c045202857a42dc
Andrew Huang <Andrew.Huang@softwire.com>
2018-11-06 16:22:22 +0000

    finished part 1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            string output;
+            for (int i = 1; i <= 100; i++)
+            {
+                output = "";
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if (output == "")
+                {
+                    output = i.ToString();
+                }
+
+                Console.WriteLine(output);
+            }
         }
     }
 }

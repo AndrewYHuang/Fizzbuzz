00415fd286cffcef406c7a8d494fe35a9431ab3f
Andrew Huang <Andrew.Huang@softwire.com>
2018-11-06 17:07:12 +0000

    Done all up to 17 reverse order

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -13,24 +13,43 @@ namespace FizzBuzz
             for (var i = 1; i <= 100; i++)
             {
                 var output = "";
-                if (i % 3 == 0) // Multiple of 3: Add Fizz
+
+                if (i % 11 == 0) // If number is multiple of 11 we do not print Fizz, Buzz or Bang
                 {
-                    output += "Fizz";
+                    if (i % 13 == 0)
+                    {
+                        output += "Fezz";
+                    }
+
+                    output += "Bong"; 
                 }
 
-                if (i % 5 == 0) // Multiple of 5: Add Buzz
+                else
                 {
-                    output += "Buzz";
-                }
+                    if (i % 3 == 0) // Multiple of 3: Add Fizz
+                    {
+                        output += "Fizz";
+                    }
 
-                if (i % 7 == 0) // Multiple of 7: Add Bang
-                {
-                    output += "Bang";
-                }
+                    if (i % 13 == 0) // Multiple of 13: add Fezz before Buzz and Bang
+                    {
+                        output += "Fezz";
+                    }
 
-                if (output == "")
-                {
-                    output = i.ToString();
+                    if (i % 5 == 0) // Multiple of 5: Add Buzz
+                    {
+                        output += "Buzz";
+                    }
+
+                    if (i % 7 == 0) // Multiple of 7: Add Bang
+                    {
+                        output += "Bang";
+                    }
+
+                    if (output == "")
+                    {
+                        output = i.ToString();
+                    }
                 }
 
                 Console.WriteLine(output);
